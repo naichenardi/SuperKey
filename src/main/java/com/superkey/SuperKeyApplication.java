c124e4ad2246ae9c58c2eb1d9074edc05498b7c1
Naiche Nardi <naiche.nardi.ext@dgeg.gov.pt>
2022-11-15 16:08:00 +0000

    Validating password and login

--- src/main/java/com/superkey/SuperKeyApplication.java
+++ src/main/java/com/superkey/SuperKeyApplication.java
@@ -15,7 +15,6 @@ public class SuperKeyApplication {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
+        return new BCryptPasswordEncoder();
     }
 }
