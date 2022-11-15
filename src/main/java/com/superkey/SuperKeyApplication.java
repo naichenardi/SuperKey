e41bd407061300983b208aa0f8454ca8041751a2
Naiche Nardi <naiche.nardi.ext@dgeg.gov.pt>
2022-11-15 15:01:50 +0000

    Implementing swagger on application

--- src/main/java/com/superkey/SuperKeyApplication.java
+++ src/main/java/com/superkey/SuperKeyApplication.java
@@ -2,15 +2,8 @@ package com.superkey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SuperKeyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuperKeyApplication.class, args);
